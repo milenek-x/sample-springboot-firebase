3d6b36fcf845a5e60e56b0fce816d76c3d9de36d
milenek-x <muneebasmone@gmail.com>
2024-12-31 04:04:56 +0530

    Initial Commit

---
+++ backend/task-microservice/src/test/java/com/example/task_microservice/TaskMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
