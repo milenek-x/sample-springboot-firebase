f36b8d38d03a0dd09b048eaf09f0bf79a74010d5
Muneeb Asmone <92797864+milenek-x@users.noreply.github.com>
2024-12-31 07:10:55 +0530

    Updated data class to integrate with the frontend

--- backend/task-microservice/src/main/java/com/example/task_microservice/data/Task.java
+++ backend/task-microservice/src/main/java/com/example/task_microservice/data/Task.java
@@ -20,4 +20,51 @@ public class Task {
     @ServerTimestamp
     public Timestamp updatedAt;     // Last updated timestamp
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
